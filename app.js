3ca356eef883bea1c96fe9be9edbed0c7768e71f
nzssmph <163339579+nzssmph@users.noreply.github.com>
2025-03-30 16:30:34 +0100

    MainPage

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainPage from './components/MainPage';  // Import the MainPage component
+
+const App = () => {
+  return <MainPage />;  // Render the MainPage component
+};
+
+export default App;
