adc48f04acd513fc556feeb07ff0b5a6ef0cf420
Sahar <sahar@Saras-MacBook-Pro.local>
2025-03-31 21:19:12 +0330

    Merged manager screen version

---
+++ components/ManageClaimsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ManageClaimsScreen = () => {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Manage Claims Screen</Text>
+		</View>
+	);
+};
+
+export default ManageClaimsScreen;
